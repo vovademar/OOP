5abeb8af41e150d094c5c6d199056c72a87b96e1
vovademarnsu <v.medvedev@g.nsu.ru>
2021-10-20 00:03:25 +0000

    modified

--- task1_3_2/src/main/java/ru/nsu/medvedev/v/Gradebook.java
+++ task1_3_2/src/main/java/ru/nsu/medvedev/v/Gradebook.java
@@ -1,11 +1,48 @@
 package ru.nsu.medvedev.v;
 
+import java.awt.print.Book;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Gradebook {
+    private int group;
+    private static int qualWork;
+    private String name;
+    final private int SEMNUM = 8;
+    private final Semester[] semester = new Semester[SEMNUM];
+
+
+    public Gradebook(int group, String name, int grade) {
+        this.group = group;
+        this.name = name;
+        qualWork = grade;
+        for (int i = 1; i <= SEMNUM; i++) {
+            Semester sem = new Semester();
+            semester[i] = sem;
+        }
+    }
+
+    public void setQualWork(int grade) {
+        qualWork = grade;
+    }
+
+    public void setGrade(int sem, int grade, String subject) {
+        semester[sem].addGrade(grade, subject);
+    }
+
+    public Collection<Integer> getSemGrade(int sem) {
+        return semester[sem].getGrades();
+    }
+
+    public ArrayList<Integer> getAllGrades() {
+        ArrayList<Integer> grades = new ArrayList<>();
+        for (int i = 0; i <= SEMNUM; i++) {
+            grades.addAll(semester[i].getGrades());
+        }
+        return grades;
+    }
 
     public static double avg_grade(ArrayList<Integer> marks) {
         double res = 0;
@@ -16,7 +53,8 @@ public class Gradebook {
         return res;
     }
 
-    public static boolean reddiploma(ArrayList<Integer> marks) {
+    public static boolean reddiploma(Gradebook gradebook) {
+        ArrayList<Integer> marks = gradebook.getAllGrades();
         double grade5_cnt = 0;
         int allcnt = 0;
         for (int i = 0; i < marks.size(); i++) {
@@ -29,24 +67,38 @@ public class Gradebook {
                 grade5_cnt++;
             }
         }
-        if (grade5_cnt / allcnt > 0.75) {
+        if ((grade5_cnt / allcnt > 0.75) && qualWork == 5) {
             return true;
         }
         return false;
     }
 
-    public static boolean moneyflex(ArrayList<Integer> marks) {
+    public boolean moneyflex(Gradebook gradebook, int sem) {
+        Collection<Integer> marks = gradebook.getSemGrade(sem);
         int grade5_cnt = 0;
         int allcnt = 0;
-        for (int i = 0; i < marks.size(); i++) {
-            if (marks.get(i) == 5) {
+        for (int mark : marks) {
+            if (mark == 5) {
                 grade5_cnt++;
                 allcnt++;
-            } else return false;
+            } else {
+                return false;
+            }
         }
-        if (allcnt == grade5_cnt) {
-            return true;
-        }
-        return false;
+        return allcnt == grade5_cnt;
+    }
+
+}
+
+class Semester {
+    Map<String, Integer> semester = new HashMap<>();
+
+    public void addGrade(int grade, String subject) {
+        this.semester.put(subject, grade);
+    }
+
+    public Collection<Integer> getGrades() {
+        return semester.values();
     }
 }
+
--- task1_3_2/src/test/java/ru/nsu/medvedev/v/GradebookTest.java
+++ task1_3_2/src/test/java/ru/nsu/medvedev/v/GradebookTest.java
@@ -1,47 +1,43 @@
 package ru.nsu.medvedev.v;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 public class GradebookTest {
+    Gradebook valdemar;
 
+    @BeforeEach
+    private void setStudent() {
+        valdemar = new Gradebook(20214, "Medvedev", 0);
+        valdemar.setGrade(1, 4, "Math");
+        valdemar.setGrade(1, 5, "ProgaC");
+        valdemar.setGrade(1, 4, "Haskellll");
+        valdemar.setGrade(1, 5, "History");
+        valdemar.setGrade(1, 4, "Logic");
+        valdemar.setGrade(2, 5, "SQL");
+        valdemar.setGrade(2, 4, "CP");
+        valdemar.setGrade(2, 5, "trees");
+    }
 
     @Test
     void avg_test() {
-        ArrayList<Integer> marks = new ArrayList<>();
-        marks.add(5);
-        marks.add(4);
+        ArrayList<Integer> marks = valdemar.getAllGrades();
         double grade = Gradebook.avg_grade(marks);
         Assertions.assertEquals(4.5, grade);
     }
 
     @Test
     void reddiploma_test() {
-        ArrayList<Integer> marks = new ArrayList<>();
-        marks.add(5);
-        marks.add(5);
-        marks.add(5);
-        marks.add(5);
-        marks.add(5);
-        marks.add(4);
-        Assertions.assertTrue(Gradebook.reddiploma(marks));
+        valdemar.setQualWork(5);
+        Assertions.assertFalse(Gradebook.reddiploma(valdemar));
     }
 
     @Test
-    void moneyflex_test_false() {
-        ArrayList<Integer> marks = new ArrayList<>();
-        marks.add(5);
-        marks.add(3);
-        Assertions.assertFalse(Gradebook.moneyflex(marks));
+    public void moneyflex_test_false() {
+        Assertions.assertFalse(valdemar.moneyflex(valdemar, 1));
     }
 
-    @Test
-    void moneyflex_test_true() {
-        ArrayList<Integer> marks = new ArrayList<>();
-        marks.add(5);
-        marks.add(5);
-        Assertions.assertTrue(Gradebook.moneyflex(marks));
-    }
 }
