e85ad78c1b40b3dcee2ea880b86e30f134d417ad
vovademarnsu <v.medvedev@g.nsu.ru>
2022-03-04 00:04:34 +0700

    Just commit

--- task2_1_1/src/main/java/ru/nsu/medvedev/v/PrimeSearch.java
+++ task2_1_1/src/main/java/ru/nsu/medvedev/v/PrimeSearch.java
@@ -1,11 +1,17 @@
 package ru.nsu.medvedev.v;
 
 import java.util.*;
-import java.util.function.*;
 import java.util.stream.*;
 
 public class PrimeSearch {
-    private boolean isPrime(int n) {
+    /**
+     * method to chek if number is prime
+     *
+     * @param n number for check
+     * @return true - if a number is prime
+     * false - if a number is non prime
+     */
+    public boolean isPrime(int n) {
         if (n <= 1)
             return false;
         else if (n == 2)
@@ -19,24 +25,101 @@ public class PrimeSearch {
         return true;
     }
 
+    /**
+     * method to check the array for prime numbers
+     *
+     * @param arr array of numbers
+     * @return true if there is prime number
+     * @return false if there is no prime numbers
+     */
     public boolean arrPrime(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            if (isPrime(arr[i])) {
+        if (arr == null) {
+            throw new NullPointerException();
+        }
+        for (int j : arr) {
+            if (isPrime(j)) {
                 return true;
             }
         }
         return false;
     }
 
+    /**
+     * method to check the array for prime number
+     *
+     * @param arr array of numbers
+     * @return true if array has non prime number
+     * @return false if there prime number
+     */
     public boolean streamPrime(int[] arr) {
-        int countPrimes = 0;
         IntStream stream = Arrays.stream(arr);
-        countPrimes = (int) stream.filter(this::isPrime).count();
-        if (countPrimes == 0) {
-            return false;
+        return stream.parallel().anyMatch(x -> !isPrime(x));
+    }
+
+    /**
+     * method to check the array for prime numbers
+     *
+     * @param arr array of numbers
+     * @return true if array has non prime number
+     * @return false if array has prime number
+     * @throws InterruptedException
+     */
+    public boolean threadPrime(int[] arr) throws InterruptedException {
+        int threadsCount = Runtime.getRuntime().availableProcessors();
+        int repeats = arr.length / threadsCount;
+        int sizeOfa = arr.length;
+        int count = 0;
+        int val = 0;
+        for (int i = 0; i <= threadsCount - 1; i++) {
+            Thread searcher;
+            if (i == threadsCount - 1) {
+                Searcher foo = new Searcher(i * repeats, sizeOfa - 1, arr, count);
+                searcher = new Thread(foo);
+                searcher.start();
+                searcher.join();
+                val += foo.getCountOfPrimes();
+            } else {
+                Searcher foo1 = new Searcher(i * repeats, i * repeats + repeats - 1, arr, count);
+                searcher = new Thread(foo1);
+                searcher.start();
+                searcher.join();
+                val += foo1.getCountOfPrimes();
+            }
+            if (val > 0) {
+                return false;
+            }
         }
         return true;
     }
 
- 
+    public class Searcher implements Runnable {
+        private int startIndex;
+        private int endIndex;
+        private int[] arrayToSearchIn;
+        private int countOfPrimes;
+
+        public Searcher(int s, int e, int[] a, int count) {
+            startIndex = s;
+            endIndex = e;
+            arrayToSearchIn = a;
+            countOfPrimes = count;
+        }
+
+        public void run() {
+            for (int i = startIndex; i <= endIndex; i++) {
+                if (isPrime(arrayToSearchIn[i])) {
+                    setCountOfPrimes(1);
+                }
+            }
+        }
+
+        public void setCountOfPrimes(int countOfPrimes) {
+            this.countOfPrimes = countOfPrimes;
+        }
+
+        public int getCountOfPrimes() {
+            return countOfPrimes;
+        }
+    }
 }
+
--- task2_1_1/src/test/java/ru/nsu/medvedev/v/PrimeTest.java
+++ task2_1_1/src/test/java/ru/nsu/medvedev/v/PrimeTest.java
@@ -1,27 +1,89 @@
 package ru.nsu.medvedev.v;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.concurrent.ExecutionException;
+
+
 public class PrimeTest {
+    PrimeSearch primeSearch;
+    @BeforeEach
+    public void initEach(){
+        primeSearch = new PrimeSearch();
+    }
     @Test
-    public void someTest(){
-        int arr[] = {1,2,3,4,5};
+    public void arrTest() {
+        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
         PrimeSearch primeSearch = new PrimeSearch();
         Assertions.assertTrue(primeSearch.arrPrime(arr));
     }
     @Test
-    public void streamTestTrue(){
-        int arr[] = {7,8,9,10,13};
+    public void arrTestLargeNum() {
+        int[] arr = {1073676287};
+        Assertions.assertTrue(primeSearch.arrPrime(arr));
+    }
+
+    @Test
+    public void arrSearchLarge() throws ExecutionException, InterruptedException {
+        int size = 1000000;
+        int[] array = new int[size];
+        Arrays.fill(array, 0, size, 1877);
+        Assertions.assertTrue(primeSearch.arrPrime(array));
+    }
+
+    @Test
+    public void streamTestFalse() {
+        int arr[] = {7,7,7};
+        PrimeSearch primeSearch = new PrimeSearch();
+        Assertions.assertFalse(primeSearch.streamPrime(arr));
+    }
+
+    @Test
+    public void streamTestTrue() {
+        int arr[] = {4, 6, 8, 10};
         PrimeSearch primeSearch = new PrimeSearch();
         Assertions.assertTrue(primeSearch.streamPrime(arr));
     }
+
     @Test
-    public void streamTestFalse(){
-        int arr[] = {4,6,8,10};
+    public void streamTest() {
+        int arr[] = {1073676287};
         PrimeSearch primeSearch = new PrimeSearch();
-        Assertions.assertFalse(primeSearch.arrPrime(arr));
+        Assertions.assertFalse(primeSearch.streamPrime(arr));
     }
 
+    @Test
+    public void streamSearchLarge() throws ExecutionException, InterruptedException {
+        int size = 1000000;
+        int[] array = new int[size];
+        Arrays.fill(array, 0, size, 1877);
+        Assertions.assertFalse(primeSearch.streamPrime(array));
+    }
+
+    @Test
+    public void threadTest() throws ExecutionException, InterruptedException {
+        int arr[] = {7,7,7};
+        PrimeSearch primeSearch = new PrimeSearch();
+        Assertions.assertFalse(primeSearch.threadPrime(arr));
+    }
+
+    @Test
+    public void threadSearchLarge() throws ExecutionException, InterruptedException {
+        int size = 1000000;
+        int[] array = new int[size];
+        Arrays.fill(array, 0, size, 1877);
+        Assertions.assertFalse(primeSearch.threadPrime(array));
+    }
+
+    @Test
+    public void threadTrue() throws ExecutionException, InterruptedException {
+        int size = 1000000;
+        int[] array = new int[size];
+        Arrays.fill(array, 0, size, 1878);
+        Assertions.assertTrue(primeSearch.threadPrime(array));
+    }
 }
